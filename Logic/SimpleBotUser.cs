cecf9c30e4d93e89492a9658c4263ee0bbad7024
JonathanBatista <36791400+JonathanBatista@users.noreply.github.com>
2018-02-24 11:33:06 -0300

    antes do almoço

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -11,20 +11,41 @@ namespace SimpleBot
     {
         private static MongoClient _cliente = new MongoClient();
 
+        private static Dictionary<string, UserProfile> _perfil = new Dictionary<string, UserProfile>();
+
         public static string Reply(Message message)
         {
-            var botResponse = $"{message.User} disse '{message.Text}'";
-            RecordMessages(message, botResponse: botResponse);
+
+            string userId = message.Id;
+
+            var perfil = GetProfile(userId);
+
+            perfil.Visitas += 1;
+
+            SetProfile(userId, perfil);
+
+            var botResponse = $"{message.User} conversou {perfil.Visitas} vezes";
+
             return botResponse;
         }
 
         public static UserProfile GetProfile(string id)
         {
-            return null;
+            if (_perfil.ContainsKey(id))
+                return _perfil[id];
+
+
+
+            return new UserProfile
+            {
+                Id = id,
+                Visitas = 0
+            };
         }
 
         public static void SetProfile(string id, UserProfile profile)
         {
+            _perfil[id] = profile;
         }
 
 
@@ -34,14 +55,12 @@ namespace SimpleBot
 
             var newDocument = new BsonDocument()
             {
-                { "userMessages", new BsonDocument(){
-                        { $"{message.User}", new BsonDocument(){
-                                { "userMessage", $"{message.Text}" },
-                                { "botMessage", botResponse },
-                            }
-                        }
+                { $"{message.User}", new BsonDocument(){
+                        { "userMessage", $"{message.Text}" },
+                        { "botMessage", botResponse },
                     }
                 }
+                    
             };
 
 
